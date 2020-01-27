d58ad030512b5da5672fa94110f6f6f9c55c12ee
Juliette <juliettebois@icloud.com>
2020-01-27 16:01:49 +0100

    :heavy_plus_sign: Question 2

--- src/main/java/com/ipiecoles/java/eval/mdd050/controllers/ArtistController.java
+++ src/main/java/com/ipiecoles/java/eval/mdd050/controllers/ArtistController.java
@@ -5,11 +5,9 @@ import com.ipiecoles.java.eval.mdd050.repositories.ArtistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -27,5 +25,11 @@ public class ArtistController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
+    }
+
+    // Question 2 : Recherche par nom
+    @GetMapping(value = "", params = "name")
+    public List<Artist> getArtistbyName(@RequestParam(value = "name") String name) {
+        return artistRepository.findAllByName(name);
     }
 }
