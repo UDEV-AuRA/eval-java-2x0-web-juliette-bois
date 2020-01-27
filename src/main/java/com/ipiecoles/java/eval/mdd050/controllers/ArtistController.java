46fc08db5e70c6d8bc171f6971232f89665007c8
Juliette <juliettebois@icloud.com>
2020-01-27 16:15:19 +0100

    :heavy_plus_sign: Question 3

--- src/main/java/com/ipiecoles/java/eval/mdd050/controllers/ArtistController.java
+++ src/main/java/com/ipiecoles/java/eval/mdd050/controllers/ArtistController.java
@@ -3,6 +3,9 @@ package com.ipiecoles.java.eval.mdd050.controllers;
 import com.ipiecoles.java.eval.mdd050.models.Artist;
 import com.ipiecoles.java.eval.mdd050.repositories.ArtistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -31,5 +34,18 @@ public class ArtistController {
     @GetMapping(value = "", params = "name")
     public List<Artist> getArtistbyName(@RequestParam(value = "name") String name) {
         return artistRepository.findAllByName(name);
+    }
+
+
+    // Question 3 : Liste des artistes
+    @GetMapping(value = "")
+    public Page<Artist> getArtistSortBy(
+            @RequestParam(value = "page") Integer page,
+            @RequestParam(value = "size") Integer size,
+            @RequestParam(value = "sortProperty") String sortProperty,
+            @RequestParam(value = "sortDirection") String sortDirection
+    ) {
+        PageRequest pageRequest= PageRequest.of(page, size, Sort.Direction.fromString(sortDirection), sortProperty);
+        return artistRepository.findAll(pageRequest);
     }
 }
